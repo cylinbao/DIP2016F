61808eda1dc1f36d7d9852e4558d007fb7d6fe9c
Chien Yu Lin <myislin@gmail.com>
2016-09-29 11:44:51 +0800

    frame for res

--- HW1/Resolution/imgQR.c
+++ HW1/Resolution/imgQR.c
@@ -1,1 +1,33 @@
-main
+#include <stdio.h>
+#include "bmp.h"
+
+#define inNum 2
+#define inNameLen 14
+#define outNum 3
+#define outNameLen 14
+
+char inNames[inNum][inNameLen] = {"../input1.bmp", "../input2.bmp"};
+char outNames[inNum][outNum][outNameLen] = {
+		 	"output1_1.bmp", "output1_2.bmp", "output1_3.bmp",
+		 	"output2_1.bmp", "output2_2.bmp", "output2_3.bmp"
+		 };
+bmp imgs[inNum * outNum];
+
+int main()
+{
+	int i, j;
+
+	for(i = 0; i < inNum; i++)
+		for(j = 0; j < outNum; j++)
+			bmpRead(inNames[i], &imgs[i*outNum + j]);
+
+	for(i = 0; i < inNum; i++)
+		for(j = 0; j < outNum; j++)
+			bmpDownRes(&imgs[i*outNum + j], j);
+
+	for(i = 0; i < inNum; i++)
+		for(j = 0; j < outNum; j++)
+		bmpWrite(outNames[i][j], &imgs[i*outNum + j]);
+
+	return 0;
+}
