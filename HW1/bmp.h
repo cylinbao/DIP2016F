a68a4a453d0baccf4560d19bb09ba1835b133350
Chien Yu Lin <myislin@gmail.com>
2016-09-30 18:19:29 +0800

    use replication for scaling up

--- HW1/bmp.h
+++ HW1/bmp.h
@@ -65,6 +65,7 @@ void bmpScaleUp(bmp *img){
 	printf("Scaling up image\n");
 
 	int i, j, k, bytePP; 
+	int x, y;
 	int oriIdx, newIdx;
 	UINT32 newDataSize = img->dataSize*4;
 	UINT32 newWidth = img->width*2;
@@ -76,16 +77,24 @@ void bmpScaleUp(bmp *img){
 
 	for(i = 0; i < newHeight; i++){
 		for(j = 0; j < newWidth; j++){
-			if((j+1)%2 == 0 && (i+1)%2 == 0){
+			if((j+1)%2 == 0 && (i+1)%2 == 0)
 				oriIdx = getIdx((j-1)/2, (i-1)/2, img->width);
-				newIdx = getIdx(j, i, newWidth);
-				setPixel(bytePP, &img->data[oriIdx*bytePP], &newData[newIdx*bytePP]);
+			else{
+				if((j+1)%2 != 0 && (i+1)%2 == 0)
+					oriIdx = getIdx((j)/2, (i-1)/2, img->width);
+				else if((j+1)%2 == 0 && (i+1)%2 != 0)
+					oriIdx = getIdx((j-1)/2, (i)/2, img->width);
+				else
+					oriIdx = getIdx((j)/2, (i)/2, img->width);
 			}
+			newIdx = getIdx(j, i, newWidth);
+			setPixel(bytePP, &img->data[oriIdx*bytePP], &newData[newIdx*bytePP]);
 		}
 	}
 
 	free(img->data);
 	img->data = newData;
+	img->fileSize += newDataSize - img->dataSize;
 	img->dataSize = newDataSize;
 	img->width = newWidth;
 	img->height = newHeight;
@@ -96,19 +105,16 @@ void bmpScaleDown(bmp *img){
 
 	int i, j, k, bytePP; 
 	int oriIdx, newIdx;
+	UINT32 newDataSize = img->dataSize/4;
 	UINT32 newWidth = img->width/2;
 	UINT32 newHeight = img->height/2;
-	UINT32 newDataSize;
-	printf("newWidth = %d, newHeight = %d\n", newWidth, newHeight);
 
-	bytePP = img->bitPerPixel / 8;
-	newDataSize = newWidth * newHeight * bytePP;
+	bytePP = img->bitPerPixel/8;
+	printf("bytePP = %d\n", bytePP);
 	BYTE *newData = malloc(newDataSize);
-
 
 	for(i = 0; i < newHeight; i++){
 		for(j = 0; j < newWidth; j++){
-			//printf("j = %d, i = %d\n", j , i);
 			oriIdx = getIdx(j*2, i*2, img->width);
 			newIdx = getIdx(j, i, newWidth);
 			setPixel(bytePP, &img->data[oriIdx*bytePP], &newData[newIdx*bytePP]);
@@ -117,6 +123,7 @@ void bmpScaleDown(bmp *img){
 
 	free(img->data);
 	img->data = newData;
+	img->fileSize -= img->dataSize - newDataSize;
 	img->dataSize = newDataSize;
 	img->width = newWidth;
 	img->height = newHeight;
@@ -139,13 +146,15 @@ void bmpRead(char *filename, bmp *img){
 	fread(&img->offset, 1, 4, inFile);
 	fread(&img->headerSize, 1, 4, inFile); assert(img->headerSize == 40);
 	fread(&img->width, 1, 4, inFile);
+	printf("width is %d\n", img->width);
 	fread(&img->height, 1, 4, inFile);
+	printf("height is %d\n", img->height);
 	fread(&img->planes, 1, 2, inFile);
 	fread(&img->bitPerPixel, 1, 2, inFile); 
-	//printf("bitPerPixel is %d\n", img->bitPerPixel);
+	printf("bitPerPixel is %d\n", img->bitPerPixel);
 	fread(&img->compression, 1, 4, inFile); assert(img->compression == 0);
 	fread(&img->dataSize, 1, 4, inFile);
-	//printf("dataSize is %d\n", img->dataSize);
+	printf("dataSize is %d\n", img->dataSize);
 	fread(&img->hResolution, 1, 4, inFile);
 	fread(&img->vResolution, 1, 4, inFile);
 	fread(&img->usedColors, 1, 4, inFile);
